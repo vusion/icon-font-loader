6acda0441e3d1713a6be098c8210b8c63d880f2c
AlfredMou <moujintao@outlook.com>
2018-01-09 20:46:02 +0800

    test 解构

--- src/loader.js
+++ src/loader.js
@@ -51,6 +51,23 @@ function iconFontLoader(source) {
     const template = handlebars.compile(plugin.options.localCSSTemplate);
     Promise.all(promises).then((results) => {
         const contents = {};
+        results.forEach((item) => {
+            const url = item.url,
+                  add = item.add,
+                  file = item.file,
+                  md5Code = item.md5Code;
+
+            // const { url, add, file, md5Code } = item;
+            let index = item.index;
+            if (add) {
+                files.push(file);
+                if (mergeDuplicates)
+                    md5s.push(md5Code);
+                index = files.length - 1;
+            }
+            // save svg font code
+            contents[url] = '\\' + (startCodepoint + index).toString(16);
+        });
         return contents;
     }).then((contents) => {
         // 第二遍replace真正替换
