c6470831eb8b28c95bd213d1eef3a73b0aff2264
AlfredMou <moujintao@outlook.com>
2018-01-09 16:53:20 +0800

    :white_check_mark: change unit test for new handleSameName function

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -2,6 +2,7 @@
  * test attribute function in plugin Object
  */
 const shell = require('shelljs');
+const fs = require('fs');
 const path = require('path');
 const Plugin = require('../src/Plugin.js');
 const IconFontPlugin = new Plugin();
@@ -30,9 +31,13 @@ describe('icon font plugin api test:', () => {
             path.resolve(__dirname, './icons/arrow-up.svg'),
         ];
         const result = IconFontPlugin.handleSameName(files);
+        const file = result[1];
+        // file.metadata = { name: 'arrow-left-1' };
+        if (!(file instanceof fs.ReadStream))
+            throw new Error('don\'t create a ReadStream for same name svg file');
         expect(result).to.eql([
             path.resolve(__dirname, './icons/arrow-left.svg'),
-            path.resolve(IconFontPlugin.tmpPath, './arrow-left-1.svg'),
+            file,
             path.resolve(__dirname, './icons/arrow-right.svg'),
             path.resolve(__dirname, './icons/arrow-up.svg'),
         ]);
