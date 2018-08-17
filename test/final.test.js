051fd358e191b5739bbf1f3f40b96adb8510757b
AlfredMou <moujintao@outlook.com>
2018-08-17 15:44:04 +0800

    :white_check_mark: add test case for mini-css-extract-plugin

--- test/final.test.js
+++ test/final.test.js
@@ -1,6 +1,7 @@
 require('./unit.test.js');
 require('./integration.test.js');
 // require('./extract-text-webpack.test.js');
+require('./mini-css-extract-plugin.test.js');
 require('./public-path.test.js');
 require('./entry.test.js');
 
---
+++ test/mini-css-extract-plugin.test.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const expect = require('chai').expect;
+const utils = require('../src/utils');
+
+const value = 'mini-css-extract-plugin';
+const replaceReg = /ICON_FONT_LOADER_IMAGE\(([^)]*)\)/g;
+
+describe('Webpack Integration Tests', () => {
+    const configPath = path.join('../test/cases/' + value + '/webpack.config.js');
+    const outputDirectory = path.join('./cases/' + value + '/dest');
+    const options = require(configPath);
+    for (const chunk of Object.keys(options.entry))
+        options.entry[chunk] = path.join(__dirname, '/cases/', value, options.entry[chunk]);
+
+    it('#test webpack css-extract case: ' + value, (done) => {
+        webpack(options, (err, stats) => {
+            if (err)
+                return done(err);
+            if (stats.hasErrors())
+                return done(new Error(stats.toString()));
+            const filesContent = fs.readFileSync(path.resolve(__dirname, outputDirectory + '/icon-font.svg'));
+            const md5Code = utils.md5Create(filesContent);
+            expect(md5Code).to.eql('8a2ce3233cbfe5ab5059f81cfc1ef039');
+            const cssContent = fs.readFileSync(path.resolve(__dirname, outputDirectory + '/bundle.css')).toString();
+            expect(replaceReg.test(cssContent)).to.eql(false);
+            done();
+        });
+    });
+});
